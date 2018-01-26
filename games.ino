f8dd6f1d547353bfd7a565369a6f0ee54c27857e
Liz Chhouk <kisaras.playground@gmail.com>
2018-01-25 19:44:21 -0500

    Final version.

--- games.ino
+++ games.ino
@@ -1,39 +1,53 @@
 #include <HX711.h>
 
 // This is the order of the scales from left to right.
-HX711 scaleBes;
-HX711 scalePtah;
-HX711 scaleSeshat;
-HX711 scaleThoth;
+HX711 scaleSeshat; //scale #1
+HX711 scaleBes;    //scale #2
+HX711 scaleThoth;  //scale #3
+HX711 scalePtah;   //scale #4
 
 float weightBes;
 float weightPtah;
 float weightSeshat;
 float weightThoth;
-const float weightThreshold = 0.01;
+const float totalWeightExpected = 750; // grams
+const float weightThreshold = 0.1;
 
-float scaleGainBes;
-float scaleGainPtah;
-float scaleGainSeshat;
-float scaleGainThoth;
+// Mathematical constants
+const int period = 32000;
+const float pi = 3.14159;
 
-// Pin assignments
-const int weighButtonPin = 2;
-const int maglockPin     = 4;
+// Pin assignments for scales
+const byte scaleSeshatDOUT  = A0;  // data1
+const byte scaleSeshatPD_SK = A1;  // clock1
+
+const byte scaleBesDOUT     = A2;  // data2
+const byte scaleBesPD_SK    = A3;  // clock2
+
+const byte scaleThothDOUT   = A4;  // data3
+const byte scaleThothPD_SK  = A5;  // clock3
+
+const byte scalePtahDOUT    = A6;  // data4
+const byte scalePtahPD_SK   = A7;  // clock4
+
+// Pin assignments for other items
+const int weighButtonPin = 45;
+const int maglockPin     = 35;
 const int redLED         = 10;
-const int greenLED       = 12;
-const int blueLED        = 14;
-
-// TODO: Delete this assignment when no longer testing.
-const int ledPin         = 13;
+const int greenLED       = 9;
+const int blueLED        = 8;
 
 int weighButtonState = 0;
+int brightness = 0;
+int fadeAmount = 5;
+float cycleCount = 0;
 
 // Function definitions
 // Check if the weights have been correctly placed.
 bool isCorrectlyPlaced() {
-  if( (weightBes < weightThreshold)    ||
-      (weightSeshat < weightThreshold) ||
+  Serial.println("Judging the things.");
+  if( (weightBes < weightThreshold)    || 
+      (weightSeshat < weightThreshold) || 
       (weightPtah < weightThreshold)   ||
       (weightThoth < weightThreshold)) {
     return false;
@@ -41,8 +55,15 @@ bool isCorrectlyPlaced() {
   return weightBes < weightSeshat && weightSeshat < weightPtah && weightPtah < weightThoth;
 }
 
+bool isValidTotalWeight() {
+  const float weightTolerance = .1; // 10%
+  const float totalWeight = weightBes + weightPtah + weightSeshat + weightThoth;
+  return (totalWeight >= totalWeightExpected*(1-weightTolerance)) && (totalWeight <= totalWeightExpected*(1+weightTolerance));
+}
+
 // Get the weights from the scales.
 void weigh() {
+  Serial.println("Weighing the things");
   weightBes    = scaleBes.get_units(5);
   weightPtah   = scalePtah.get_units(5);
   weightSeshat = scaleSeshat.get_units(5);
@@ -50,120 +71,74 @@ void weigh() {
 }
 
 void setup() {
-  HX711 scales [4] = {scaleBes, scalePtah, scaleSeshat, scaleThoth};
-
   Serial.begin(38400);
 
-  Serial.print("Initializing the buttons and lights...");
+  Serial.print("Initializing the buttons and lights...\n");
   pinMode(weighButtonPin, INPUT_PULLUP);
   pinMode(maglockPin, OUTPUT);
   pinMode(redLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
   pinMode(blueLED, OUTPUT);
 
-  //TO DO: Delete this initializiation when no longer testing.
-  pinMode(ledPin, OUTPUT);
+  digitalWrite(maglockPin, LOW);
 
-  Serial.print("Initializing the scales...");
+  Serial.print("Initializing the scales...\n");
   // Parameter 'gain' omitted from the invocations;
   // default value 128 is used by the HX711 library.
 
-  // Pins for scaleBes:
-  // HX711.DOUT  - pin #A0
-  // HX711.PD_SK - pin #A1
-  scaleBes.begin(A0, A1);
+  scaleBes.begin(scaleBesDOUT, scaleBesPD_SK);
+  scalePtah.begin(scalePtahDOUT, scalePtahPD_SK);
+  scaleSeshat.begin(scaleSeshatDOUT, scaleSeshatPD_SK);
+  scaleThoth.begin(scaleThothDOUT, scaleThothPD_SK);
 
-  // Pins for scalePtah:
-  // HX711.DOUT  - pin #A3
-  // HX711.PD_SK - pin #A4
-  scalePtah.begin(A3, A4);
+  // set_scale() takes a float and uses it to convert the raw data to "human readable" data with units.
+  scaleBes.set_scale(212.6260575296);
+  scaleBes.tare();
+  
+  scalePtah.set_scale(209.350253807);
+  scalePtah.tare();
+  
+  scaleSeshat.set_scale(210.441624365);
+  scaleSeshat.tare();
+  
+  scaleThoth.set_scale(215.59390862944);
+  scaleThoth.tare();
 
-  // Pins for scaleSeshat:
-  // HX711.DOUT  - pin #A6
-  // HX711.PD_SK - pin #A7
-  scaleSeshat.begin(A6, A7);
-
-  // Pins for scaleThoth:
-  // HX711.DOUT  - pin #A9
-  // HX711.PD_SK - pin #A10
-  scaleThoth.begin(A9, A10);
-
-  // Calibrate scaleBes only.
-  // After the gain has been confirmed, we can calibrate the other scales.
-  // Because the game requires relative weights only,
-  // we only need to calibrate the scales once.
-  // TODO: Replace the rest of setup() with --
-  //    scaleBes.set_scale(gain.f);
-  //    scaleBes.tare();
-  //    scalePtah.set_scale(gain.f);
-  //    scalePtah.tare();
-  //    scaleSeshat.set_scale(gain.f);
-  //    scaleSeshat.tare();
-  //    scaleThoth.set_scale(gain.f);
-  //    scaleThoth.tare();
-
-  Serial.print("Setting up scaleBes...");
-  Serial.print("read: \t\t");
-  Serial.println(scaleBes.read());             // print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scaleBes.read_average(20));   // print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scaleBes.get_value(5));       // print the average of 5 readings from the ADC minus the tare weight (not set yet)
-
-  Serial.print("get units: \t\t");
-  Serial.println(scaleBes.get_units(5), 1);    // print the average of 5 readings from the ADC minus tare weight (not set) divided
-                                               // by the SCALE parameter (not set yet)
-
-  scaleBes.set_scale(2280.f);                  // this value is obtained by calibrating the scale with known weights; see the README for details
-                                               // The value that is passed into `set_scale` will define the units for the output.
-  scaleBes.tare();                             // reset the scale to 0
-
-  Serial.println("Confirming setup of scaleBes:");
-
-  Serial.print("read: \t\t");
-  Serial.println(scaleBes.read());              // print a raw reading from the ADC
-
-  Serial.print("read average: \t\t");
-  Serial.println(scaleBes.read_average(20));    // print the average of 20 readings from the ADC
-
-  Serial.print("get value: \t\t");
-  Serial.println(scaleBes.get_value(5));        // print the average of 5 readings from the ADC minus the tare weight, set with tare()
-
-  Serial.print("get units: \t\t");
-  Serial.println(scaleBes.get_units(5), 1);     // print the average of 5 readings from the ADC minus tare weight, divided
-                                                // by the SCALE parameter set with set_scale
 }
 
 void loop() {
   weighButtonState = digitalRead(weighButtonPin);
 
-  // TODO: "Activate" the actual code once we're no longer testing.
-
+  // Default state.
+  analogWrite(greenLED, 70+64*sin((2*pi*cycleCount/period)));
+  analogWrite(redLED, 132+122*sin((2*pi*cycleCount/period)));
+  
+  cycleCount++;
+  if(cycleCount > period){
+    cycleCount = 0;
+  }
+  
   if (weighButtonState == HIGH) {
-    Serial.print("'Weigh' button has been pressed!");
-    // weigh();
-    // if (isCorrectlyPlaced()) {
-    //   turn LED green:
-    //   digitalWrite(greenLED, HIGH);
-    //   digitalWrite(redLED, LOW);
-    //   deactivate the maglock:
-    //   digitalWrite(maglockPin, LOW);
-    //   delay(5000);
-    // } else {
-    //   turn LED red:
-    //   digitalWrite(redLED, HIGH);
-    //   digitalWrite(greenLED, LOW);
-    //   delay(1000);
-    // }
-    digitalWrite(ledPin, HIGH);
-    delay(1000);
-  } else {
-    digitalWrite(ledPin, LOW);
-    // turn on LED ready light:
-    // digitalWrite(redLED, HIGH);
-    // digitalWrite(greenLED, HIGH);
-    delay(1000);
+    Serial.println("WEIGH button has been pressed.");
+    // Once the WEIGH button has been pressed, judge the gifts.
+    weigh();
+    if (isCorrectlyPlaced() && isValidTotalWeight()) {
+      Serial.println("Hooray.");
+      // The weights have the correct relative weights.
+      // Turn the LED green.
+      digitalWrite(greenLED, HIGH);
+      digitalWrite(redLED, LOW);
+
+      // Deactivate the magnetic lock.
+      digitalWrite(maglockPin, HIGH);
+      delay(5000);
+    } else {
+      Serial.println("Un-hooray.");
+      // The gifts do not have the correct relative weights.
+      // Turn the LED red.
+      digitalWrite(redLED, HIGH);
+      digitalWrite(greenLED, LOW);
+      delay(5000);
+    }
   }
 }
